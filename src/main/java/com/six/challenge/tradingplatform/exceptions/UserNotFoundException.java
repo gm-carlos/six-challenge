2548156279e882fefc83d5cd0cb69bd8b4e79298
Carlos <carlos@Carloss-iMac.local>
2022-11-04 18:00:27 +0100

    Add error simple handling

--- src/main/java/com/six/challenge/tradingplatform/exceptions/UserNotFoundException.java
+++ src/main/java/com/six/challenge/tradingplatform/exceptions/UserNotFoundException.java
@@ -1,7 +1,10 @@
 package com.six.challenge.tradingplatform.exceptions;
 
-import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.UUID;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(UUID id) {
